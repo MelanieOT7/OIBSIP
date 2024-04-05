2f003ce3c04bcfd86362cafe68a03987b251dd07
Nontando <carolnontando@gmail.com>
2024-04-05 04:02:10 +0200

    created an if statement to redirect to dashboard or clear password if wrong credentials are added

--- Login authentication/login.js
+++ Login authentication/login.js
@@ -3,4 +3,11 @@ function loginUser(username, password) {
   const user = users.find(
     (user) => user.email === username && user.password === password
   );
+  if (user) {
+    alert("Login successful. Redirecting to dashboard.");
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid username or password. Please try again.");
+    document.getElementById("password").value = "";
+  }
 }
