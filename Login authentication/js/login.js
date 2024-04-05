9b1de5afc9bf59d9db5ab6049af6239c220ad7db
Nontando <carolnontando@gmail.com>
2024-04-05 03:52:10 +0200

    added a form variable  and to  run class if fileds are validated

--- Login authentication/js/login.js
+++ Login authentication/js/login.js
@@ -67,3 +67,9 @@ class Login {
 
   //end
 }
+
+const form = document.querySelector(".loginForm");
+if (form) {
+  const fields = ["username", "password"];
+  const validator = new Login(form, fields);
+}
