1e8dd6551adbbf10eb7f1ed248b5d3326d8bcb7b
Nontando <carolnontando@gmail.com>
2024-04-05 04:00:52 +0200

    used find method to find user with matching credentials

--- Login authentication/login.js
+++ Login authentication/login.js
@@ -1,3 +1,6 @@
 function loginUser(username, password) {
   const users = JSON.parse(localStorage.getItem("users")) || [];
+  const user = users.find(
+    (user) => user.email === username && user.password === password
+  );
 }
