3b3ff6325e660f1cd4940917cef2334250e9eaac
Nontando <carolnontando@gmail.com>
2024-04-05 03:39:18 +0200

    added attributes to the validate function and handles error

--- Login authentication/js/login.js
+++ Login authentication/js/login.js
@@ -5,7 +5,25 @@ class Login {
     this.validateonSubmit();
   }
 
-  validateonSubmit() {}
+  validateonSubmit() {
+    let self = this;
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      var error = 0;
+
+      self.fields.forEach((field) => {
+        const input = document.querySelector(`#${field}`);
+        if (self.validateFields(input) == false) {
+          error++;
+        }
+      });
+
+      if (error == 0) {
+        localStorage.setItem("auth", 1);
+        this.form.submit();
+      }
+    });
+  }
 
   //end
 }
